d95ba0bec287d92818b0fc6424816880083a3b7a
deethompson <devonthompson.sl@gmail.com>
2020-04-20 15:04:57 -0400

    second commit

--- src/App.js
+++ src/App.js
@@ -9,15 +9,42 @@ const Header = () => (
 );
 
 class App extends Component {
+  // with create react app we can write our methods as arrow
+  //functions, meaning we don't need the constructor and bind
+
+  handleChange = event =>{
+    const {value} = event.target
+    if (value.length > 2) {
+     
+    }
+  };
+
+  handleyKeyPress = event => {
+    const {value} = event.target;
+
+    if (value.length > 2 && event.key === 'Enter') {
+      alert(`search for ${value}`)
+    }
+
+
+  };
+
+ 
   render() {
     return (
       <div className="page">
 
         <Header />
-      
-
- 
-      </div>
+        <div className='search grid'>
+        {/* stack of gif images */}
+        <input className='input grid-item' placeholder='Type something'
+        onChnage={this.handleChange}
+        onKeyPress={this.handleKeyPress}
+        />
+        
+        
+        </div>
+         </div>
     );
   }
 }
