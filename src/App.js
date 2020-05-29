d383d4b12bfc82b16467f100a802419c7e576a2e
deethompson <devonthompson.sl@gmail.com>
2020-05-29 13:48:21 -0400

    update async feature

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 //we need to import our components from 
+import loader from './images/loader.svg'
+
 const Header = () => (
 <div className="header grid">
   <h1 className="title">jiffy</h1>
@@ -8,12 +10,45 @@ const Header = () => (
 
 );
 
+const UserHint = ({loading, hintText}) => (
+<div className='user-hint'>
+ 
+  {loading ? <img className="block mx-auto" src={loader}/> :
+
+hintText}
+
+ </div>
+
+);
+
 class App extends Component {
-  // with create react app we can write our methods as arrow
-  //functions, meaning we don't need the constructor and bind
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchTerm: '',
+      hintText: 'Hit Enter to search'
+
+    };
+  }
+  //we want a function that searches the giphy api using 
+  // fetch and puts the search term into the query url and 
+  // then we can do something with the results 
+
+  searchGiphy = async searchTerm => {
+
+    
+  }
+
+  
 
   handleChange = event => {
     const {value} = event.target;
+    this.setState((prevState, props) => ({
+      // we take our old props  and spread them out here
+      ...prevState,
+      // and then we overwrite the ones we want after 
+      searchTerm: value
+    }));
     if (value.length > 2) {
      
     }
@@ -23,12 +58,14 @@ class App extends Component {
     const {value} = event.target
     
     if (value.length > 2 && event.key === 'Enter') {
-      alert(`search for ${value}`)
+      alert(`search for ${value}`);
     }
 
   };
  
   render() {
+    // const searchTerm = this.state.searchTerm
+    const { searchTerm } = this.state 
     return (
       <div className="page">
 
@@ -40,10 +77,13 @@ class App extends Component {
         placeholder="Type something"
         onChange={this.handleChange}
         onKeyPress={this.handleKeyPress}
+        value= {searchTerm}
         />
         
         
         </div>
+
+        <UserHint />
          </div>
     );
   }
